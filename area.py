220c04849afa7dd8104c61b1d5a41efd784104cf
James Houck <jameshouck@live.com>
2019-09-19 12:27:48 -0500

    added new function to area.py - to get circle area

--- area.py
+++ area.py
@@ -1,6 +1,12 @@
+import math
 def triangle_area(height, base):
     
     if base < 0 or height < 0:
         raise ValueError("Cannot have a negative value.")
     
     return height * base * 0.5
+
+def circle_area(radius):
+    if radius < 0:
+        raise ValueError
+    return radius ** 2 * math.pi
